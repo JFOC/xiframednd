2bcac9b1f36405c7505497e8255b35f53a924b52
Matt Naus <mattijs@chillyorange.com>
2016-09-09 14:47:41 +0700

    Must work with mutiple dyn generated iframes

--- js/custom.js
+++ js/custom.js
@@ -2,36 +2,41 @@ $(function () {
 
 	var li,
 		iframe,
+		x,
 		ul = document.getElementById('frames');
 
-	li = document.createElement('LI');
+	for ( x = 0; x < 2; x++ ) {
 
-	iframe = document.createElement('IFRAME');
-	iframe.src = "frame1.html";
-	iframe.setAttribute('frameborder', 0);
-	iframe.setAttribute("scolling", "no");
-	iframe.id = "frame1";
+		li = document.createElement('LI');
 
-	$(iframe).on('load', function () {
+		iframe = document.createElement('IFRAME');
+		iframe.src = "frame1.html";
+		iframe.setAttribute('frameborder', 0);
+		iframe.setAttribute("scolling", "no");
+		iframe.id = "frame1";
 
-		$('li', document.getElementById('mainDraggables')).draggable({
-			connectToSortable: $('#frames iframe').contents().find('ul').sortable({
-				revert: true,
-				iframefix: true,
-	            cancel: '',
-	            helper: 'clone',
-	            placeholder: "drop-hover",
-	            connectWith: $('#frames iframe').contents().find('ul'),
-			}),
-			helper: "clone",
-	        revert: "invalid",
-	        iframefix: true,
-	        appendTo: document.body
+		$(iframe).on('load', function () {
+
+			$('li', document.getElementById('mainDraggables')).draggable({
+				connectToSortable: $('#frames iframe').contents().find('ul').sortable({
+					revert: true,
+					iframefix: true,
+		            cancel: '',
+		            helper: 'clone',
+		            placeholder: "drop-hover",
+		            connectWith: $(iframe).contents().find('ul'),
+				}),
+				helper: "clone",
+		        revert: "invalid",
+		        iframefix: true,
+		        appendTo: document.body
+			});
+
 		});
 
-	});
+		li.appendChild(iframe);
+		ul.appendChild(li);
 
-	li.appendChild(iframe);
-	ul.appendChild(li);
+	}
 
 });
